4a73cf9a07c176ac6a53c87fd93d230202f07969
Meow Roar <sareacct91@gmail.com>
2023-12-30 16:05:46 -0800

    Added drag and drop functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
 const shortcutModal = new bootstrap.Modal(document.getElementById('shortcutModal'));
 
+// DOM selectors
+const linksListEl = document.getElementById('linksList');
+const nameInputEl = document.getElementById('nameInput');
+const urlInputEl = document.getElementById('urlInput');
+
+
 document.getElementById('inputForm').addEventListener('submit', (evt) => {
   evt.preventDefault();
-
-  // DOM selectors
-  const linksListEl = document.getElementById('linksList');
-  const nameInputEl = document.getElementById('nameInput');
-  const urlInputEl = document.getElementById('urlInput');
 
   // Get user input
   const name = nameInputEl.value;
@@ -26,13 +27,13 @@ document.getElementById('inputForm').addEventListener('submit', (evt) => {
 
   // Create the li element and insert to the page
   const htmlStr = `
-    <li>
-      <a href="${url}" target="_blank" class="card col align-center">
-        <img src='https://icons.duckduckgo.com/ip3/${favIcoUrl}.ico' alt="" />
-        <span>${name}</span>
+    <li id="${name}" draggable="true">
+      <button class="delete-btn" draggable="false">x</button>
+      <a href="${url}" class="card col align-center">
+        <img src='https://icons.duckduckgo.com/ip3/${favIcoUrl}.ico' alt="" draggable="false"/>
+        ${name}
       </a>
-    </li>
-    `;
+    </li>`;
   linksListEl.insertAdjacentHTML('beforeend', htmlStr);
 
   // Close the modal
@@ -43,5 +44,66 @@ document.getElementById('inputForm').addEventListener('submit', (evt) => {
 });
 
 addEventListener('DOMContentLoaded', () => {
-  document.getElementById('linksList').innerHTML = localStorage.getItem("shortcuts") || '';
+  linksListEl.innerHTML = localStorage.getItem("shortcuts") || '';
+
+  // Delete button event handler
+  linksListEl.addEventListener('click', (evt) => {
+    // If not a delete button exit out
+    if (!evt.target.matches('.delete-btn')) {
+      return;
+    }
+    // Delete the parent element ie. li tag
+    evt.target.parentElement.remove();
+
+    // Save the list to localStorage
+    localStorage.setItem("shortcuts", linksListEl.innerHTML);
+  })
+
+  // Draging and Drop API
+  linksListEl.addEventListener('dragstart', (evt) => {
+    // Store the dragged element
+    evt.dataTransfer.setData('text/plain', evt.target.parentElement.id);
+    evt.dataTransfer.setDragImage(evt.target.parentElement, 0, 0);
+    evt.target.classList.add('dragging');
+  });
+
+  // Set up the dragover event handler to allow the drop
+  linksListEl.addEventListener('dragover', (evt) => {
+    // Prevent the default behavior to allow dropping
+    evt.preventDefault();
+    evt.target.classList.add('dragover');
+  });
+
+  // Set up the dragleave event handler to remove the highlight
+  linksListEl.addEventListener('dragleave', (evt) => {
+    evt.target.classList.remove('dragover');
+  });
+
+  // Set up the drop event handler
+  linksListEl.addEventListener('drop', (evt) => {
+    // Prevent the default behavior to avoid the file being opened
+    evt.preventDefault();
+
+    // Get the data from the transfer
+    const data = evt.dataTransfer.getData('text/plain');
+    const source = document.getElementById(data);
+
+    if (evt.target.matches('ul')) {
+      evt.target.insertAdjacentElement('beforeend', source)
+    }
+    else {
+      // Find the dropped element
+      const droppedEl = evt.target.closest('li');
+      // Insert the dragged element before the dropped element
+      droppedEl.insertAdjacentElement('beforebegin', source);
+    }
+
+    // Remove the highlight
+    evt.target.classList.remove('dragover');
+  });
+
+  // Delete the original element
+  linksListEl.addEventListener('dragend', (evt) => {
+    evt.target.classList.remove('dragging');
+  });
 })
